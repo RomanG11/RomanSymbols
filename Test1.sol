1c77b6f206e81d65aea84bc98f4f97a95b9b29f8
Roman <33402067+RomanG11@users.noreply.github.com>
2018-03-26 17:28:22 +0300

    Add files via upload

--- Test1.sol
+++ Test1.sol
@@ -7,7 +7,6 @@ contract RomanNumbers {
     
     using strings for *;
     
-	string public re;
 
 	/*
 	1		I	лат. unus, unum
@@ -25,27 +24,30 @@ contract RomanNumbers {
 			res = res.toSlice().concat("M".toSlice());
 			decimal -= 1000;
 		}
-		while (decimal >= 900){
-			res = res.toSlice().concat("CM".toSlice());
-			decimal -= 900;
-		}
 		while (decimal >= 500) {
+			if(decimal >= 900) {
+				res = res.toSlice().concat("CM".toSlice());
+				decimal -= 900;
+				break;
+			}
         	res = res.toSlice().concat("D".toSlice());
         	decimal -= 500;
     	}
-    	while (decimal >= 400) {
-        	res = res.toSlice().concat("CD".toSlice());
-        	decimal -= 400;
-    	}
     	while (decimal >= 100) {
+    		if (decimal >= 400){
+    			res = res.toSlice().concat("CD".toSlice());
+        		decimal -= 400;
+        		break;
+    		}
         	res = res.toSlice().concat("C".toSlice());
         	decimal -= 100;
     	}
-    	while (decimal >= 90) {
-        	res = res.toSlice().concat("XC".toSlice());
-        	decimal -= 90;
-    	}
    		while (decimal >= 50) {
+   			if(res >= 90){
+   				res = res.toSlice().concat("XC".toSlice());
+        		decimal -= 90;
+        		break;
+   			}
         	res = res.toSlice().concat("L".toSlice());
         	decimal -= 50;
     	}
@@ -75,4 +77,22 @@ contract RomanNumbers {
     	}
     	return res;
 	}
+
+	uint[] public arab = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+	string[] public roman = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
+
+
+	function ConvertToRomanNumbers2 (uint decimal) public view returns(string res) {
+		uint iterator = arab[arab.length-1];
+		while (decimal > 0){
+			if(decimal >= arab[iterator]){
+				res = res.toSlice().concat(roman[iterator].toSlice());
+				decimal -= arab[iterator];
+			}
+			else{
+				iterator--;
+			}
+		}
+	}
+	
 }
